8afd8629e742cb19f44c5911f59c08a9b142e028
Abhishek Sinha <aabhisheksinah@gmail.com>
2021-06-17 21:03:36 +0530

    adding toolbar

--- gdocs/src/Editor.js
+++ gdocs/src/Editor.js
@@ -1,6 +1,21 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 import Quill from 'quill';
 import "quill/dist/quill.snow.css";
+
+const TOOL = [
+    [{ header : [1,2,3,4,5,6,false]}],
+    [{font : [] }],
+    [{ list : "ordered"}, {list : "bullet"}],
+    [{ script : "sub" },{ script : "super"}],
+    [{ color : [] }, {background:[]}],
+    ["bold", "italic","underline"],
+    [{ align : [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+
+]
+
+
 const Editor = () => {
     const wrapperRef = useCallback(wrapper =>{
         if(wrapper == null){
@@ -9,7 +24,7 @@ const Editor = () => {
         wrapper.innerHTML = ""
         const workspace  = document.createElement('div')
         wrapper.append(workspace)
-        new Quill(workspace,{theme:'snow'})
+        new Quill(workspace,{theme:'snow', modules:{toolbar: TOOL}})
         
     },[])
     return (
--- gdocs/src/index.js
+++ gdocs/src/index.js
@@ -6,7 +6,7 @@ function App(){
     return(
         
         <div>
-            Hello there
+            {/* Hello there */}
             <Editor />
         </div>
     )
